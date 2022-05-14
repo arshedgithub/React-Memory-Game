a958e7a508fd5ebeb2eb2dac53a61ca0752609f1
Arshed Ahmed <arshedahmedmail@gmail.com>
2022-05-14 14:13:04 +0530

    shuffle cards

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./App.css";
 
 const cardImages = [
@@ -8,11 +9,25 @@ const cardImages = [
   { src: "/img/shield-1.png" },
   { src: "/img/sword-1.png" },
 ];
+
 function App() {
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+
+  // shuffle cards
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }));
+    setCards(shuffledCards);
+    setTurns(0);
+  };
+  console.log(cards, turns);
+
   return (
     <div className="App">
       <h1>Magic Match</h1>
-      <button>New Game</button>
+      <button onClick={shuffleCards}>New Game</button>
     </div>
   );
 }
