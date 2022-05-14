f5383a3c0fddaa91a1f9d03fd66b0e8b07c3a5b1
Arshed Ahmed <arshedahmedmail@gmail.com>
2022-05-14 19:11:16 +0530

    flipping cards

--- src/App.js
+++ src/App.js
@@ -46,8 +46,12 @@ function App() {
             }
           });
         });
+        resetTurn();
+      } else {
+        setTimeout(() => {
+          resetTurn();
+        }, 1000);
       }
-      resetTurn();
     }
   }, [choiceOne, choiceTwo]);
 
@@ -64,7 +68,12 @@ function App() {
       <button onClick={shuffleCards}>New Game</button>
       <div className="card-grid">
         {cards.map((card) => (
-          <SingleCard key={card.id} card={card} handleChoice={handleChoice} />
+          <SingleCard
+            key={card.id}
+            card={card}
+            handleChoice={handleChoice}
+            flipped={card === choiceOne || card === choiceTwo || card.matched}
+          />
         ))}
       </div>
     </div>
